1374e6e174a30d03dd3c9640b9b5c55032dd7fd1
EmiyaredA <49465479+EmiyaredA@users.noreply.github.com>
2019-04-14 22:57:53 +0800

    1
    1

---
+++ enemy_Attack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Attack : MonoBehaviour
+{
+    public Transform shotspawn;
+    public GameObject shot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Instantiate(shot, shotspawn.position, shotspawn.rotation) as GameObject;
+        GameObject.Destroy(go, 3f);
+    }
+}
