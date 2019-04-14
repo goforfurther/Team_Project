d56f58d6183631896b3d4cfef603044509729e43
EmiyaredA <49465479+EmiyaredA@users.noreply.github.com>
2019-04-14 22:59:17 +0800

    3
    3

---
+++ enemy_attack02.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_attack02 : MonoBehaviour
+{
+    private Transform player;
+    private Animator anim;
+    public float movespeed = 1.0f;
+    bool iswalking = true;
+    private float time = 0;
+    // Start is called before the first frame update
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("shot"))
+        {
+            anim.SetBool("was_hit", true);
+            Destroy(other, 3.0f);
+        }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (iswalking)
+        {
+            anim.SetBool("iswaking", false);
+        }
+        anim.SetBool("iswaking", true);
+        if(Time.time-time>10.0f)
+        {
+            anim.SetBool("player_exist", false);
+            time = Time.time;
+        }
+        if (Time.time - time > 4.0f)
+        {
+            anim.SetBool("player_exist", true);
+        }
+    }
+}
